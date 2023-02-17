b36889b18356b173b77b0c00aa4e002bb804356d
Rose Gekonge <rgekonge2001@gmail.com>
2023-02-17 15:48:51 -0800

    0-positive or negative

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+##include <stdio.h>
+
+/**
+ * main - Prints the numbers from 1 to 10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+        for (x = 0; x <= 9; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
