6ea155e4509be83c0700dd142ba21f34dcad453a
Rose Gekonge <rgekonge2001@gmail.com>
2023-02-19 13:45:23 -0800

    printing doubles

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print numbers without doubles
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int r;
+	int g = 0;
+
+	while (g < 10)
+	{
+		r = 0;
+		while (r < 10)
+		{
+			if (g != r && g < r)
+			{
+				putchar('0' + g);
+				putchar('0' + r);
+
+				if (r + g != 100)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			r++;
+		}
+		g++;
+	}
+	putchar('\n');
+	return (0);
+}
