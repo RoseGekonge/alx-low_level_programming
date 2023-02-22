2c68a267a0f9e9b3a54ef4b0cc0843ee19171e51
Rose Gekonge <rgekonge2001@gmail.com>
2023-02-22 15:28:21 -0800

    absolute

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,17 +3,19 @@
 #include "main.h"
 
 /**
- * print_sign - prints sign
+ * _abs - prints sign
  *
  * @n: number with sign
  *
  * Return: 1 if greater than zero, 0 if equal to zero, -1 if less than zero
  */
 
-int _abs(int k)
+int _abs(int n)
 {
-	if (k < 0)
-		return ("%d\n" , k * -1);
-	else
-		return ("%d\n" , k);
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
 }
+
