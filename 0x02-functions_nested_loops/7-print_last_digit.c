8dc43fec156dc641e08403880d9f4b4e3a535bc8
Rose Gekonge <rgekonge2001@gmail.com>
2023-02-22 15:34:42 -0800

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,16 +12,18 @@
 
 int print_last_digit(int k)
 {
-	int g;
+	int a;
 
 	if (k < 0)
-	{
-		g = -k % 10;
-	}
-	else if (k < 0)
-	{
-		g = k % 10;
-	}
-	_putchar(g + '0');
-	return (g);
+	k = -k;
+
+	a = k % 10;
+
+	if (a < 0)
+		a = -a;
+
+	_putchar(a + '0');
+
+	return (a);
 }
+
