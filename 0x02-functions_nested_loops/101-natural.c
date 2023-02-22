55da648e11c0e88e5cd7eb576010ddfb99daf138
Rose Gekonge <rgekonge2001@gmail.com>
2023-02-22 15:00:19 -0800

    sum of divisibles

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - prints sum of divisibles
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
+
