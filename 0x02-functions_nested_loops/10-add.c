18b22b2404d788dff22709319fe0831064801aa1
Rose Gekonge <rgekonge2001@gmail.com>
2023-02-22 14:16:35 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * add - prints sign
+ *
+ * @r: number r
+ * @g: number g
+ *
+ * Return: returns sum of the numbers
+ */
+
+int add(int r, int g)
+{
+	int sum = r + g;
+
+	return (sum);
+}
