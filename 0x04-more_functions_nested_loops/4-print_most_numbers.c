6d5250f262ce75fad36a75ba09e1d84370364167
Rose Gekonge <rgekonge2001@gmail.com>
2023-02-23 11:18:16 -0800

    not 2 or 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - prints 0 to 9
+ * Return: nothing
+ */
+
+void print_most_numbers(void)
+{
+	printf("%d", 0);
+	printf("%d", 1);
+	printf("%d", 3);
+	printf("%d", 5);
+	printf("%d", 6);
+	printf("%d", 7);
+	printf("%d", 8);
+	printf("%d", 9);
+	printf("\n");
+}
