462663853ebcf7123c8bb296f73acaa6708e00ad
Rose Gekonge <rgekonge2001@gmail.com>
2023-03-06 12:29:10 -0800

    set to to s

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -5,12 +5,10 @@
 /**
  * set_string - sets the value of a pointer to a char.
  * @to: first pointer
- * @*s: second pointer
+ * @s: second pointer
  */
 
 void set_string(char **s, char *to)
 {
-	s = &to;
-
-	printf("%s", s);
+	*s = to;
 }
