4160d7c01d38d1f594494c07dbd0e54d617d2af8
Rose Gekonge <rgekonge2001@gmail.com>
2023-03-08 03:52:50 -0800

    printing s

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: string input
+ */
+
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,2 @@
+             0X08-RECURSION
+1. 0-puts recursion.c
