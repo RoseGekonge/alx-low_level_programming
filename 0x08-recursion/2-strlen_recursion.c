7861931603118f2f87b2d86ee00339374aac3dfc
Rose Gekonge <rgekonge2001@gmail.com>
2023-03-08 05:29:01 -0800

    length of string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * _strlen_recursion - returns length
+ * @s: string to count
+ * Return: length
+ */
+
+int _strlen_recursion(char *s)
+{
+	return (strlen(s));
+}
