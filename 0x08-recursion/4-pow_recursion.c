06fc142468c60d581d275f51a39315b4960d4193
Rose Gekonge <rgekonge2001@gmail.com>
2023-03-08 06:55:22 -0800

    x to the power of y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _pow_recursion - prints x to the power of y
+ * @x: base number
+ * @y: power value
+ * Return: the result of x to the power of y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (pow(x, y));
+	}
+}
