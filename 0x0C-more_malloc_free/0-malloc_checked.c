0f5976b0b6573a9ec9807314fa6ae4fcc20fa047
Rose Gekonge <rgekonge2001@gmail.com>
2023-03-16 15:35:39 -0700

    malloc check

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - function
+ * @b: variable
+ * Return: result
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int NUM = 98;
+
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit(NUM);
+	}
+	printf("%p", ptr);
+}
