c9191ff430e1a5ab4a05cf4fa7d0965db546bf84
Rose Gekonge <rgekonge2001@gmail.com>
2023-03-16 17:19:06 -0700

    using calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * memset_do - memset
+ * @s: .....
+ * @b: .....
+ * @n: .....
+ * Return: s
+ */
+
+char *memset_do(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
+
+/**
+ * *_calloc - allocates memory for an array
+ * @nmemb: number of elements in the array
+ * @size: size of each element
+ *
+ * Return: pointer to allocated memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	ptr = malloc(size * nmemb);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	_memset(ptr, 0, nmemb * size);
+	return (ptr);
+}
