d4f668d3d259b3cdfea1038b271d7edaa2be5e80
Rose Gekonge <rgekonge2001@gmail.com>
2023-03-20 15:12:50 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog
+ * @name:name
+ * @age: age
+ * @owner: owner
+ * Description; about the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
