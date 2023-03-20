a8e3455153e66086b99aeb886068dbd088d77aab
Rose Gekonge <rgekonge2001@gmail.com>
2023-03-20 10:36:11 -0700

    print filename

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - prints the file name
  * Return: name of file
  */
 
-int main()
+int main(void)
 {
 	printf("%s\n", __FILE__);
-	return(0);
+	return (0);
 }
