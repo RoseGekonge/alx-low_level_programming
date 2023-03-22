88e6f66890ed2bccd55902bc6e32e72bb1670e1b
Rose Gekonge <rgekonge2001@gmail.com>
2023-03-22 20:49:04 +0300

    exectuting function called action

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * array_iterator - executes action
+ * @array: array
+ * @size: size of array
+ * @action: pointer to function to execute
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int r;
+
+	for (r = 0; r < size; r++)
+	{
+		action(array[r]);
+	}
+}
