70aa5d5b67b5215004ebc3dbad2c1f1cdcc165fe
Rose Gekonge <rgekonge2001@gmail.com>
2023-03-24 02:18:32 +0300

    separator

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - returns calculation results
+ * @separator: string to be printed
+ * @n: length og arguments
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int len, r;
+	va_list ap;
+
+	if (separator != NULL)
+	{
+		len = n;
+
+		va_start(ap, n);
+		for (r = 0; r < len; r++)
+		{
+			printf("%d%s" va_arg(ap, int), separator);
+		}
+		va_end(ap);
+		printf("\n");
+	}
+	else
+	{
+		printf(" ");
+	}
+}
