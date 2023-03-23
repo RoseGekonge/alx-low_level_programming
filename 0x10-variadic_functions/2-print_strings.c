1042d2e917420519f0967c273fcceed3859962f8
Rose Gekonge <rgekonge2001@gmail.com>
2023-03-24 02:55:24 +0300

    separate strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_strings - separates strings with a separator
+ * @separator: separates strings
+ * @n: number of arguments
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...);
+{
+	int len, r;
+	va_list ap;
+
+	len = n;
+	if (separator != NULL && len != 0)
+	{
+		va_start(ap, n);
+		if (len == 1)
+		{
+			printf("%s", va_arg(ap, *char));
+		}
+		for (r = 0; r < len; r++)
+		{
+			printf("%s%s", va_arg(ap, *char), separator);
+		}
+		va_end(ap);
+		printf("\n");
+	}
+	else
+	{
+		printf(" ");
+	}
+}
