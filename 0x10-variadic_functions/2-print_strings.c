c1bf648b88efff08d94904d385f28a10cabbf013
Rose Gekonge <rgekonge2001@gmail.com>
2023-03-24 04:19:52 +0300

    separate strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,10 +16,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	if (separator != NULL && len != 0)
 	{
 		va_start(ap, n);
-		if (va_arg(ap, char *) == NULL)
-		{
-			printf("%s", "(nil)");
-		}
 		if (len == 1)
 		{
 			printf("%s", va_arg(ap, char *));
