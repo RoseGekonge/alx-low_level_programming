4900cf705499ce8fd8d5555d2e327e6127ea3170
RoseGekonge <rgekonge2001@gmail.com>
2023-03-30 21:22:19 +0300

    return count

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all elements
+ * @h: argument
+ *
+ * Return: elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	int count = 0;
+
+	const list_t *node = h;
+
+	while (node != NULL)
+	{
+		count++;
+		node = node->next;
+	}
+	return (count);
+}
