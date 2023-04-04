d47723d95d4112d8d98f01c6d28dd1bfe5bd8d41
RoseGekonge <rgekonge2001@gmail.com>
2023-04-04 23:03:36 +0300

    free and null

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees list and sets the head to NULL
+ * @head: argument
+ * Return: count
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *node;
+
+	while (head != NULL)
+	{
+		node = *head;
+		free(node);
+		*head = NULL;
+	}
+}
