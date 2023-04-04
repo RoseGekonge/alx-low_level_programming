6ea8f529201e0aeccd842d80ecb31453bf4b3a69
RoseGekonge <rgekonge2001@gmail.com>
2023-04-05 02:31:10 +0300

    insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,7 +24,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			*head = temp;
 			return (temp);
 		}
-		if ((r + 1)== idx)
+		if ((r + 1) == idx)
 		{
 			temp->n = n;
 			temp->next = node->next;
