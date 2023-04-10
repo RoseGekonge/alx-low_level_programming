73508e14411300cea404adb1500c3e94a40a655c
RoseGekonge <rgekonge2001@gmail.com>
2023-04-10 23:26:57 +0300

    binary from decimal

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,58 +1,31 @@
 #include "main.h"
 
 /**
- * isEven - checks if od or even
- * @num: number to check
- * Return: 0 or 1
- */
-
-int isEven(int num)
-{
-	if ((num & 1) == 0)
-	{
-		return (0);
-	}
-	else
-	{
-		return (1);
-	}
-}
-
-/**
- * for_print_binary - converts decimal to binary
- * @n: decimal number
- * Return: binary
- */
-
-int for_print_binary(unsigned long int n)
-{
-	long int num = 0, f, s, b;
-	long int m = 0, x = 10, g, i = 1;
-
-	s = isEven(n);
-	while (n > 1)
-	{
-		n >>= 1;
-		b = isEven(n);
-		while (num < (i - 1))
-		{
-			x *= 10;
-			num++;
-		}
-		f = b * x;
-		m += f;
-		i++;
-	}
-	g = m + s;
-	return (g);
-}
-
-/**
  * print_binary - converts decimal to binary
- * @n: decimal number
+ * @n: number to convert
  */
 
 void print_binary(unsigned long int n)
 {
-	for_print_binary(n);
+	unsigned long int r;
+	int k = 0, g;
+
+	for (g = 63; g >= 0; g--)
+	{
+		r = n >> g;
+
+		if (r & 1)
+		{
+			k++;
+			_putchar('1');
+		}
+		else if (k)
+		{
+			_putchar('0');
+		}
+	}
+	if (!k)
+	{
+		_putchar('0');
+	}
 }
