7076669865d7c1eb0ccf7385609e50618c7f4825
RoseGekonge <rgekonge2001@gmail.com>
2023-04-14 01:12:24 +0300

    copy file a to file b

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,43 +16,36 @@ int main(int ac, char **av)
 	if (ac != 3)
 	{
 		dprintf(2, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		exit(97); }
 	r = fopen(av[1], "r");
 	if (r == NULL || av[1] == NULL)
 	{
 		dprintf(2, "Error: Can't read from file NAME_OF_THE_FILE\n");
-		exit(98);
-	}
+		exit(98); }
 	k = open(av[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
 	if (k == -1)
 	{
 		dprintf(2, "Error: Can't write to NAME_OF_THE_FILE\n");
-		exit(99);
-	}
+		exit(99); }
 	g = fread(arr, sizeof(char), 1024, r);
 	if (g == -1)
 	{
 		dprintf(2, "Error: Can't read from file NAME_OF_THE_FILE\n");
-		exit(98);
-	}
+		exit(98); }
 	j = write(k, arr, g);
 	if (j == -1)
 	{
 		dprintf(2, "Error: Can't write to NAME_OF_THE_FILE\n");
-		exit(99);
-	}
+		exit(99); }
 	fclose(r);
 	if (fclose(r) == -1)
 	{
 		dprintf(2, "Error: Can't close fd FD_VALUE\n");
-		exit(100);
-	}
+		exit(100); }
 	close(k);
 	if (close(k) == -1)
 	{
 		dprintf(2, "Error: Can't close fd FD_VALUE\n");
-		exit(100);
-	}
+		exit(100); }
 	return (0);
 }
