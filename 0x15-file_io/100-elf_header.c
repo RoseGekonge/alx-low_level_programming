581db7a36310abd0304768c4c322d1ba3935e042
RoseGekonge <rgekonge2001@gmail.com>
2023-04-14 03:24:08 +0300

    elf

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,5 +1,47 @@
 #include "main.h"
 
+#define SIZEOF_EHDR  sizeof(Elf64_Ehdr)
+#define ERR_ELF      "Error: Unable to read ELF header.\n"
+#define ERR_FILE     "Error: Unable to open file.\n"
+#define ERR_USAGE    "Usage: %s <ELF file>\n"
+
+/**
+ * print_elf_header - prints header
+ * @ehdr: argument
+ */
+
+void print_elf_header(Elf64_Ehdr *ehdr)
+{
+	int i;
+
+	printf("ELF Header:\n");
+	printf("  Magic:   ");
+	for (i = 0; i < EI_NIDENT; i++)
+	{
+		printf("%02x ", ehdr->e_ident[i]); }
+	printf("\n");
+	printf("  Class:                             %s\n", ehdr->e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "ELF64");
+	printf("  Data:                              %s\n", ehdr->e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "");
+	printf("  Version:                           %d (current)\n", ehdr->e_ident[EI_VERSION]);
+	printf("  OS/ABI:                            ");
+	switch (ehdr->e_ident[EI_OSABI])
+	{
+		case ELFOSABI_SYSV:
+			printf("UNIX - System V\n");
+			break;
+		case ELFOSABI_NETBSD: printf("UNIX - NetBSD\n"); break;
+		default:              printf("<unknown>\n");     break; }
+	printf("  ABI Version:                       %d\n", ehdr->e_ident[EI_ABIVERSION]);
+	printf("  Type:                              ");
+	switch (ehdr->e_type)
+	{
+	case ET_EXEC:  printf("EXEC (Executable file)\n");   break;
+	case ET_DYN:   printf("DYN (Shared object file)\n"); break;
+	default:       printf("<unknown>\n");                break;
+	}
+	printf("  Entry point address:               0x%lx\n", ehdr->e_entry);
+}
+
 /**
  * main - main function
  * @argc: number of arguments
@@ -9,41 +51,30 @@
 
 int main(int argc, char **argv)
 {
-	int i, fd;
-	Elf64_Ehdr elf_header;
-	
+	int fd;
+	ssize_t n;
+	Elf64_Ehdr ehdr;
+
 	if (argc != 2)
-	{ fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
-		exit(98); }
+	{
+		fprintf(stderr, ERR_USAGE, argv[0]);
+		exit(EXIT_FAILURE);
+	}
 	fd = open(argv[1], O_RDONLY);
-	if(fd == -1)
-	{ fprintf(stderr,"Error: Cannot open file %s\n", argv[1]);
-		exit(98); }
-	if(lseek(fd, 0, SEEK_SET) == -1)
-	{ fprintf(stderr, "Error: Cannot set file offset\n");
-		exit(98); }
-	if (read(fd, &elf_header, sizeof(Elf64_Ehdr)) != sizeof(Elf64_Ehdr))
-	{ fprintf(stderr, "Error: Cannot read ELF header\n");
-		exit(98); }
-	if (elf_header.e_ident[EI_MAG0] != ELFMAG0 || 
-			elf_header.e_ident[EI_MAG1] != ELFMAG1 ||
-			elf_header.e_ident[EI_MAG2] != ELFMAG2 ||
-			elf_header.e_ident[EI_MAG3] != ELFMAG3)
-	{ fprintf(stderr,"Error: %s is not an ELF file\n", argv[1]);
-		exit(98); }
-	printf("Magic:   ");
-	for (i = 0; i < EI_NIDENT; i++)
-	{ printf("%02x ", elf_header.e_ident[i]); }
-	printf("\n");
-	printf("Class:                             %s\n", elf_header.e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "ELF64");
-	printf("Data:                              %s\n", elf_header.e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
-	printf("Version:                           %d\n", elf_header.e_ident[EI_VERSION]);
-	printf("OS/ABI:                            %d\n", elf_header.e_ident[EI_OSABI]);
-	printf("ABI Version:                       %d\n", elf_header.e_ident[EI_ABIVERSION]);
-	printf("Type:                              %d\n", elf_header.e_type);
-	printf("Entry point address:               %lx\n", elf_header.e_entry);
-	if( close(fd) == -1)
-	{ fprintf(stderr, "Error: Cannot close file descriptor\n");
-		exit(98); }
-	return (0);
+	if (fd < 0)
+	{
+		fprintf(stderr, ERR_FILE);
+		exit(EXIT_FAILURE);
+	}
+	n = read(fd, &ehdr, SIZEOF_EHDR);
+	if (n != SIZEOF_EHDR)
+	{
+		fprintf(stderr, ERR_ELF);
+		close(fd);
+		exit(EXIT_FAILURE);
+	}
+	print_elf_header(&ehdr);
+
+	close(fd);
+	exit(EXIT_SUCCESS);
 }
