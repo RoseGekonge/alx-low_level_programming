192538299aacaccfeb813d41bb5f6095919458e2
RoseGekonge <rgekonge2001@gmail.com>
2023-08-17 16:19:22 +0300

    returns number of elements in a list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * dlistint_len - returns number of elements in a list
+ * @h: list to chech
+ * Return: number of elements in a list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int r;
+
+	while (h != NULL)
+	{
+		r++;
+		h = h->next
+	}
+	return (r);
+}
