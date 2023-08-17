821fca695d1bf856e86dbba0282706dd266b87b6
RoseGekonge <rgekonge2001@gmail.com>
2023-08-17 16:08:14 +0300

    printing a doubly linked list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -5,10 +5,13 @@
  * @h: list to print
  * Return: 0
  */
-size_t print_dlistint(const dlistint_t *h);
+size_t print_dlistint(const dlistint_t *h)
 {
+	int i = 0;
 	while (h != NULL){
-		print("%d", h->n);
-		h = h->next
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
 	}
+	return (i);
 }
