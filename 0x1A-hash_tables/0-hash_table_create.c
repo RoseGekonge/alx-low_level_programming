37fee84274640397117545eaa7923cc89bdebd23
RoseGekonge <rgekonge2001@gmail.com>
2023-09-08 17:52:38 +0300

    function thaon that creates a new hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,26 +1,27 @@
 #include "hash_tables.h"
 
-/*
+/**
  * hash_table_create - creates a hash table
  * @size: size of array in the hash table
  * Return: a pointer to the hash table created
  */
-
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
-	hash_table = malloc(sizeof(hash_table_t));
-	if (hash_table == NULL)
+	int i;
+
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
 		return (NULL);
 	table->size = size;
-	table->*array = malloc(size * sizeof(hash_node_t *));
-	if (hash_table->array == NULL)
+	table->array = malloc(size * sizeof(hash_node_t *));
+	if (table->array == NULL)
 	{
-		free(hash_table);
+		free(table);
 		return (NULL);
 	}
-	for(i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 		table->array[i] = NULL;
 
-	return (table)
+	return (table);
 }
