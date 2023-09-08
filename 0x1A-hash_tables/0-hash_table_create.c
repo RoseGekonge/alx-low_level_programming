d2a111fa96fdc05bc36ff89bf78a64f960f95024
RoseGekonge <rgekonge2001@gmail.com>
2023-09-08 17:41:57 +0300

    function thaon that creates a new hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,7 +6,7 @@
  * Return: a pointer to the hash table created
  */
 
-hash_table_t *hash_table_create(unsigned long int size);
+hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
 	hash_table = malloc(sizeof(hash_table_t));
